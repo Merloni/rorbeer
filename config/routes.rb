3d9feb6d1ef3e558d0139d3194d5717d469207e2
Merloni <tuomo.salmenkivi@gmail.com>
2016-02-21 14:05:06 +0200

    enviroment fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resource :session, only: [:new, :create, :destroy]
 
+
+  get 'places', to: 'places#index'
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
