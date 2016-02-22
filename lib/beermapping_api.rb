e59c2bf72a77a7390e8fac0a0133eaee513756e5
Merloni <tuomo.salmenkivi@gmail.com>
2016-02-22 12:43:05 +0200

    QKJWEGHDJAHWEJK2

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,19 +1,19 @@
 class BeermappingApi
   def self.places_in(city)
-    url = 'http://stark-oasis-9187.herokuapp.com/api/'
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-
-
     places = response.parsed_response["bmp_locations"]["location"]
 
-    if places.is_a?(Hash) and places['id'].nil?
-      redirect_to places_path, :notice => "No places in #{city]}"
-    else
-      places = [places] if places.is_a?(Hash)
-      @places = places.map do | location |
-        Place.new(location)
-      end
-      render :index
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do | place |
+      Place.new(place)
     end
+  end
+
+  def self.key
+    "603a900090cba4c23b6d3beba8c3828c"
   end
 end
