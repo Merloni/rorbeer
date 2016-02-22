ccbeb916b85f84c7efbb52b7bf0c345e3c5f2a8b
Merloni <tuomo.salmenkivi@gmail.com>
2016-02-22 12:59:04 +0200

    fix bois

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,7 @@ class PlacesController < ApplicationController
   end
 
   def search
+    
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations found in #{params[:city]}"
